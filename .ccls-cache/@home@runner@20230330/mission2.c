6f7fbe468433b3783ce95eb43fd28130da0b767b
seoyeon30629 <129129762+seoyeon30629@users.noreply.github.com>
2023-05-04 01:12:01 +0000

    미션2.점수를 입력받으면 그 점수에 맞는 학점을 출력하는 프로그램을 배웠다.

---
+++ .ccls-cache/@home@runner@20230330/mission2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main(){
+  int a;
+  
+  printf("점수를 입력:");
+  scanf("%d", &a);
+
+  if(a>=90)
+printf("A학점");
+  else if(a>=80)
+    printf("B학점");
+  else if(a>=70)
+    printf("C학점");
+  else if(a>=60)
+    printf("D학점");
+  else
+    printf("F학점");
+}
